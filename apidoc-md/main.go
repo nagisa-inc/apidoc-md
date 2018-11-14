290bc844f4511628c3be1b7a2e8ed60eaa304d8a
nagisa-oikawa <oikawa@nagisa-inc.jp>
2018-11-14 14:56:57 +0900

    fix: main.go

--- apidoc-md/main.go
+++ apidoc-md/main.go
@@ -1,7 +1,56 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/urfave/cli"
+)
 
 func main() {
-	fmt.Println("apidoc-md")
+	app := cli.NewApp()
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "input, i",
+			Value: "",
+			Usage: "input `DIR` path",
+		},
+		cli.StringFlag{
+			Name:  "output, o",
+			Value: "",
+			Usage: "output `FILE` path",
+		},
+	}
+	app.Action = func(c *cli.Context) error {
+		// input
+		inputDirPath, err := filepath.Abs(c.String("i"))
+		if err != nil {
+			return err
+		}
+		info, err := os.Stat(inputDirPath)
+		if err != nil {
+			return err
+		}
+		if !info.IsDir() {
+			return fmt.Errorf("input path is not directory")
+		}
+
+		// output
+		outputPath, err := filepath.Abs(c.String("o"))
+		if err != nil {
+			return err
+		}
+
+		fmt.Println(inputDirPath)
+		fmt.Println(outputPath)
+
+		//return exec(inputDirPath, outputPath)
+		return nil
+	}
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+		os.Exit(1)
+	}
 }
