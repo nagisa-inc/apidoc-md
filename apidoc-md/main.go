65af540ff4eb52a603193dad5302fcf369ac8d46
nagisa-oikawa <oikawa@nagisa-inc.jp>
2019-11-12 18:17:36 +0900

    fix client library

--- apidoc-md/main.go
+++ apidoc-md/main.go
@@ -7,67 +7,65 @@ import (
 
 	"github.com/nagisa-inc/apidoc-md"
 
-	"github.com/urfave/cli"
+	"github.com/spf13/cobra"
 )
 
+var rootCmd = &cobra.Command{
+	Short:   "Convert apiDoc (http://apidocjs.com) documentation to markdown",
+	Version: "1.0.0",
+	RunE:    run,
+}
+
+var (
+	input  string
+	output string
+)
+
+func init() {
+	rootCmd.PersistentFlags().StringVarP(&input, "input", "i", "", "input apidoc `DIR` path")
+	rootCmd.PersistentFlags().StringVarP(&output, "output", "o", "", "output markdown `FILE` path")
+}
+
+func run(_ *cobra.Command, _ []string) error {
+	// input
+	if input == "" {
+		return fmt.Errorf("--input option is required")
+	}
+	inDirPath, err := filepath.Abs(input)
+	if err != nil {
+		return err
+	}
+	inInfo, err := os.Stat(inDirPath)
+	if err != nil {
+		return err
+	}
+	if !inInfo.IsDir() {
+		return fmt.Errorf("input path is not directory")
+	}
+
+	// output
+	if output == "" {
+		return fmt.Errorf("--output option is required")
+	}
+	outPath, err := filepath.Abs(output)
+	if err != nil {
+		return err
+	}
+	outInfo, err := os.Stat(outPath)
+	if err != nil {
+		if !os.IsNotExist(err) {
+			return err
+		}
+	} else {
+		if outInfo.IsDir() {
+			return fmt.Errorf("output path is directory")
+		}
+	}
+	return apidocmd.Convert(inDirPath, outPath)
+}
+
 func main() {
-	app := cli.NewApp()
-	app.Usage = ""
-	app.Description = "Convert apiDoc (http://apidocjs.com) documentation to markdown"
-	app.Version = "1.0.0"
-	app.Flags = []cli.Flag{
-		&cli.StringFlag{
-			Name:  "input, i",
-			Usage: "input apidoc `DIR` path",
-		},
-		&cli.StringFlag{
-			Name:  "output, o",
-			Usage: "output markdown `FILE` path",
-		},
-	}
-	app.Action = func(c *cli.Context) error {
-		// input
-		inOpt := c.String("i")
-		if inOpt == "" {
-			return fmt.Errorf("--input option is required")
-		}
-		inDirPath, err := filepath.Abs(inOpt)
-		if err != nil {
-			return err
-		}
-		inInfo, err := os.Stat(inDirPath)
-		if err != nil {
-			return err
-		}
-		if !inInfo.IsDir() {
-			return fmt.Errorf("input path is not directory")
-		}
-
-		// output
-		outOpt := c.String("o")
-		if outOpt == "" {
-			return fmt.Errorf("--output option is required")
-		}
-		outPath, err := filepath.Abs(outOpt)
-		if err != nil {
-			return err
-		}
-		outInfo, err := os.Stat(outPath)
-		if err != nil {
-			if !os.IsNotExist(err) {
-				return err
-			}
-		} else {
-			if outInfo.IsDir() {
-				return fmt.Errorf("output path is directory")
-			}
-		}
-
-		return apidocmd.Convert(inDirPath, outPath)
-	}
-
-	// run
-	if err := app.Run(os.Args); err != nil {
+	if err := rootCmd.Execute(); err != nil {
 		fmt.Printf("[error] %s\n", err)
 		os.Exit(1)
 	}
--- go.mod
+++ go.mod
@@ -4,5 +4,5 @@ go 1.12
 
 require (
 	github.com/deckarep/golang-set v1.7.1
-	github.com/urfave/cli v1.20.0
+	github.com/spf13/cobra v0.0.5
 )
